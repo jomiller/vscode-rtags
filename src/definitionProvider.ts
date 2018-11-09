c29b23bb6f44b58e190ff1b9c5e8329ff1e3d39d
Jonathan Miller <jonmiller99@outlook.com>
2018-11-09 13:58:26 -0500

    Fix "Show Variables" for const/volatile variable definitions

--- src/definitionProvider.ts
+++ src/definitionProvider.ts
@@ -55,7 +55,7 @@ function getBaseSymbolType(symbolType: string, unscoped: boolean, isConstructor:
     }
     if (unscoped)
     {
-        pattern += "|^((\\w+::)+)";
+        pattern += "|((\\w+::)+)";
     }
     const baseSymbolRegex = new RegExp(pattern, "g");
     const baseSymbolType = symbolType.replace(baseSymbolRegex, "");
