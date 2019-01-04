jomiller/vscode-rtags

commit 44ece6607df3ee5c7030e984736551b1763cda35

Move wildcard symbol to the call sites to clarity how wildcards are used

src/symbolProvider.ts
@@ -85,7 +85,7 @@ function findSymbols(query: string, args: string[] = []) : Promise<Optional<Symb
     const localArgs =
     [
         "--find-symbols",
-        query + '*',
+        query,
         "--wildcard-symbol-names",
         "--match-icase",
         "--strip-paren",
@@ -185,7 +185,7 @@ export class RtagsSymbolProvider implements
             document.uri.fsPath
         ];
 
-        return findSymbols("", args);
+        return findSymbols('*', args);
     }
 
     public provideWorkspaceSymbols(query: string, _token: CancellationToken) : ProviderResult<SymbolInformation[]>
@@ -197,7 +197,7 @@ export class RtagsSymbolProvider implements
 
         if (query.length > 3)
         {
-            return this.findWorkspaceSymbols('*' + query);
+            return this.findWorkspaceSymbols('*' + query + '*');
         }
 
         const resolveCallback =
@@ -208,10 +208,10 @@ export class RtagsSymbolProvider implements
                     return Promise.resolve(symbols);
                 }
 
-                return this.findWorkspaceSymbols('*' + query);
+                return this.findWorkspaceSymbols('*' + query + '*');
             };
 
-        return this.findWorkspaceSymbols(query).then(resolveCallback);
+        return this.findWorkspaceSymbols(query + '*').then(resolveCallback);
     }
 
     private async findWorkspaceSymbols(query: string) : Promise<SymbolInformation[]>

commit bc9b460141e5ad4c7859231a9a8b1afe42ad4f82

Fix regression introduced in 44ece6607df3ee5c7030e984736551b1763cda35
Exclude file symbols from document symbol searches

src/symbolProvider.ts
@@ -80,7 +80,8 @@ function toSymbolKind(kind: string) : Optional<SymbolKind>
     return undefined;
 }
 
-function findSymbols(query: string, args: string[] = []) : Promise<Optional<SymbolInformation[]>>
+function findSymbols(query: string, args: string[] = [], includeFiles: boolean = false) :
+    Promise<Optional<SymbolInformation[]>>
 {
     const localArgs =
     [
@@ -113,7 +114,7 @@ function findSymbols(query: string, args: string[] = []) : Promise<Optional<Symb
                 let symbolKind: Optional<SymbolKind> = undefined;
                 if (!name)
                 {
-                    if (query.length === 0)
+                    if (!includeFiles)
                     {
                         continue;
                     }
@@ -233,7 +234,7 @@ export class RtagsSymbolProvider implements
                 maxSearchResults.toString()
             ];
 
-            const symbols = await findSymbols(query, args);
+            const symbols = await findSymbols(query, args, true);
             if (symbols)
             {
                 workspaceSymbols.push(...symbols);