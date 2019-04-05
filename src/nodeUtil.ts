jomiller/vscode-rtags

commit 95db71bb693fd36727b2e0c8d241c827dd7c9f94

Treat internal compilation database directories as external directories when the recursive search is disabled

src/nodeUtil.ts
@@ -66,9 +66,11 @@ export function makeAbsolutePath(base: string, fsPath: string) : string
     return path.resolve(base, fsPath);
 }
 
-export function isContainingDirectory(parent: string, sub: string) : boolean
+export function isContainingDirectory(parent: string, path: string, orEqual: boolean = false) : boolean
 {
-    return sub.startsWith(addTrailingSeparator(parent));
+    const dir = addTrailingSeparator(parent);
+    const isEqual = (addTrailingSeparator(path) === dir);
+    return ((!orEqual && path.startsWith(dir) && !isEqual) || (orEqual && isEqual));
 }
 
 export function findFiles(path: string, includePattern: string, excludePattern?: string | ReadonlyArray<string>) :

src/rtagsManager.ts
@@ -579,7 +579,7 @@ function getProjectRoots() : Promise<Optional<Uri[]>>
 
 function validateProjectRoot(parentRoot: string, subRoot: string) : void
 {
-    if ((parentRoot !== subRoot) && isContainingDirectory(parentRoot, addTrailingSeparator(subRoot)))
+    if (isContainingDirectory(parentRoot, subRoot))
     {
         throw new Error("Nested project roots are not supported. Project root " + subRoot +
                             " is a subdirectory of project root " + parentRoot);
@@ -898,7 +898,7 @@ async function validateProject(workspacePath: Uri,
         {
             targetProjectRoot = projectRoot;
 
-            if (!isContainingDirectory(targetProjectRoot.fsPath, addTrailingSeparator(workspacePath.fsPath)))
+            if (!isContainingDirectory(targetProjectRoot.fsPath, workspacePath.fsPath, true))
             {
                 throw new Error(
                     "The workspace folder must be within the target project root: " + targetProjectRoot.fsPath);
@@ -990,11 +990,16 @@ async function validateProject(workspacePath: Uri,
         {
             const isInternalCompileDirectory =
                 currentWorkspaceCompileInfo && currentWorkspaceCompileInfo.recursiveSearchEnabled &&
-                isContainingDirectory(currentCompileBaseDirectory.fsPath, addTrailingSeparator(info.directory.fsPath));
+                isContainingDirectory(currentCompileBaseDirectory.fsPath, info.directory.fsPath);
 
             if (isInternalCompileDirectory || (info.directory.fsPath === currentCompileBaseDirectory.fsPath))
             {
                 currentCompileDirectories.push(info.directory);
+
+                if (isInternalCompileDirectory && !targetWorkspaceCompileInfo.recursiveSearchEnabled)
+                {
+                    currentExternalCompileDirectoryExists = true;
+                }
             }
             else
             {

commit 1db6f7410335a34bdfdbff9d1e46ac24c7441e0b

Fix logic error introduced in 95db71b

src/nodeUtil.ts
@@ -70,7 +70,7 @@ export function isContainingDirectory(parent: string, path: string, orEqual: boo
 {
     const dir = addTrailingSeparator(parent);
     const isEqual = (addTrailingSeparator(path) === dir);
-    return ((!orEqual && path.startsWith(dir) && !isEqual) || (orEqual && isEqual));
+    return ((path.startsWith(dir) && !isEqual) || (orEqual && isEqual));
 }
 
 export function findFiles(path: string, includePattern: string, excludePattern?: string | ReadonlyArray<string>) :